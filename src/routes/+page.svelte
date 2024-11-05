3187fad156d5b85762bf7f83372937b323c2c503
Anna Mockenhaupt <anna.mockenhaupt@koeln.de>
2024-11-05 18:20:37 +0100

    scroll down animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,7 @@
       <div class="title">
         <h1>Geburtstagsfestival</h1>
         <h1>8.11 - 11.11</h1>
+        <div class="scroll-down">hier gehts lang v</div>
       </div>
     </div>
 
@@ -71,5 +72,32 @@
     color: #fcf1d9;
     gap: 20px;
     margin-top: -60px;
+  }
+
+  .scroll-down {
+    position: fixed;
+    bottom: 40px;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 1.5rem;
+    color: white;
+    text-shadow: 1px 1px 2px black;
+    animation: bounce 2s infinite;
+  }
+
+  @keyframes bounce {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+      transform: translateX(-50%) translateY(0);
+    }
+    40% {
+      transform: translateX(-50%) translateY(-10px);
+    }
+    60% {
+      transform: translateX(-50%) translateY(-5px);
+    }
   }
 </style>
